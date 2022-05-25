c0b6ad81e2052472d6ed2cec8b55f8c2e5135665
Vanessa <vvasquez96662@umanizales.edu.co>
2022-05-24 20:38:40 -0500

    clase

--- parcial.py
+++ parcial.py
@@ -1,2 +1,11 @@
-print("hola")
-print("hola")
+from inspect import _void
+from Persona import Persona
+
+class Jugador:
+    NumFed: int
+    
+
+    def __init__(self, Persona, Nombre, nif, fechaNac):
+        self.Nombre = Persona
+        self.nif = Persona
+        self.fechaNac = Persona
